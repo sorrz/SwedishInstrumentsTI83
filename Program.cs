38f66f22e5dbcc214057e04a98185ddb491c4d99
Dan Stjärnborg <dan.stjarnborg@gmail.com>
2022-10-14 10:27:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SwedishInstrumentsTI83
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
